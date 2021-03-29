56cfe072ea84ba860842688177fae057f4a1045a
Chassigneux <chassigneuxjb@gmail.com>
2021-03-29 23:45:38 +0200

    'typescript'

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,55 @@
 "use strict";
-const express = require('express');
-const app = express();
-app.get("/", (req, res) => {
-    res.send("mescouilles");
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`);
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv = __importStar(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
+const morgan_1 = __importDefault(require("morgan"));
+const app = express_1.default();
+const dotenvRes = dotenv.config({ path: '.env' });
+// Environment variables
+if (dotenvRes.error)
+    console.log(`[ENV FILE ERROR] ${dotenvRes.error}`);
+// Database connexions
+mongoose_1.default.connect(`${process.env.DB_HOST}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log('[DATABASE CONNEXION STATUS] SUCCESS'); })
+    .catch((e) => { console.log(`[DATABASE CONNEXION STATUS] ${e}`); });
+mongoose_1.default.set('useCreateIndex', true);
+// Middleware
+app.use(helmet_1.default());
+app.use(cors_1.default());
+if (process.env.NODE_ENV !== 'production')
+    app.use(morgan_1.default('dev'));
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+// Routes
+app.use('/', (req, res, next) => {
+    res.status(200).end("mescouilles");
+    next();
 });
+app.listen(`${process.env.PORT}`, () => { console.log(`Server listening on port ${process.env.PORT}`); });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,37 @@
-const express = require('express');
-const app = express();
-app.get("/", (req : any, res : any): void => {
-    res.send("mescouilles")
-})
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${PORT}`)
-})
+import express from 'express';
+import * as dotenv from "dotenv";
+import mongoose from 'mongoose';
+import bodyParser from "body-parser";
+import cors from 'cors';
+import helmet from 'helmet';
+import logger from 'morgan';
+
+
+const app: express.Application = express();
+const dotenvRes: dotenv.DotenvConfigOutput = dotenv.config({ path: '.env' });
+
+// Environment variables
+if(dotenvRes.error) console.log(`[ENV FILE ERROR] ${dotenvRes.error}`);
+
+// Database connexions
+mongoose.connect(`${process.env.DB_HOST}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {console.log('[DATABASE CONNEXION STATUS] SUCCESS')})
+    .catch((e) => {console.log(`[DATABASE CONNEXION STATUS] ${e}`)})
+mongoose.set('useCreateIndex', true);
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+
+if(process.env.NODE_ENV !== 'production') app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/', (req, res, next) => {
+    res.status(200).end("mescouilles");
+    next();
+});
+
+app.listen(`${process.env.PORT}`, () => {console.log(`Server listening on port ${process.env.PORT}`)});
