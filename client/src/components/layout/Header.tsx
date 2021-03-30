f5bec0cee23b5d1a8211bda944f65f7649a7102b
Chassigneux <chassigneuxjb@gmail.com>
2021-03-30 11:08:19 +0200

    'knacss

---
+++ client/src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type HeaderProps = {
+    title: string,
+    paragraph: string,
+}
+
+export const Header: React.FunctionComponent<HeaderProps> = ({title, paragraph}) => {
+    return <div>sd</div>
+}
