c09e33874a73791e60921b3a24ed595c3f9f4c63
Chassigneux <chassigneuxjb@gmail.com>
2021-03-31 20:23:48 +0200

    structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './scss/main.scss';
+
+import Header from './components/layout/Header';
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+    <div className="musicplace">
+        <Router>
+            <Header />
+            <Switch>
+                <Route exact path='/'><Home /></Route>
+                <Route exact path='/ofertas'>Ofertas</Route>
+                <Route exact path='/cursos'>Cursos</Route>
+                <Route exact path='/conciertos'>Conciertos</Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
-type HeaderProps = {
-    title: string,
-    paragraph: string,
+const Header: React.FunctionComponent = () => {
+    return (
+        <header>
+            <div className={'container'}>
+                <div className={'mosaic'} data-valign={'center'}>
+                    <h1 className={'logo-font my-0'}><Link to={'/'}>MusicPlace</Link></h1>
+                    <div><Link to={'/ofertas'}>Ofertas</Link></div>
+                    <div><Link to={'/cursos'}>Cursos</Link></div>
+                    <div><Link to={'/conciertos'}>Conciertos</Link></div>
+                    <div data-dock={'right'}><Link to={'/login'}>login</Link></div>
+                </div>
+            </div>
+        </header>
+    )
 }
 
-export const Header: React.FunctionComponent<HeaderProps> = ({title, paragraph}) => {
-    return <div>sd</div>
-}
+export default Header;
