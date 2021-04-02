4b0332cc15a609089d04ca44a042cae571b23b7c
Chassigneux <chassigneuxjb@gmail.com>
2021-04-02 14:36:32 +0200

    'FA'

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header: React.FunctionComponent = () => {
     return (
         <header>
             <div className={'container'}>
                 <div className={'mosaic'} data-valign={'center'}>
-                    <h1 className={'logo-font my-0'}><Link to={'/'}>MusicPlace</Link></h1>
-                    <div><Link to={'/ofertas'}>Ofertas</Link></div>
-                    <div><Link to={'/cursos'}>Cursos</Link></div>
-                    <div><Link to={'/conciertos'}>Conciertos</Link></div>
-                    <div data-dock={'right'}><Link to={'/login'}>login</Link></div>
+                    <h1 className={'logo-font my-0'}><Link to={'/'}>M</Link></h1>
+                    <nav data-amplitud={'grow'}>
+                        <div className={'mosaic'}>
+                            <div className={'ml-4'}><Link to={'/ofertas'}>Ofertas</Link></div>
+                            <div><Link to={'/cursos'}>Cursos</Link></div>
+                            <div><Link to={'/conciertos'}>Conciertos</Link></div>
+                            <div data-dock={'right'}>
+                                <Link to={'/login'}>
+                                    <FontAwesomeIcon icon={['fas', 'user']} />
+                                </Link>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </header>
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import '../../scss/pages/home.scss';
 
 const Home: React.FunctionComponent = () => {
     return(
         <div className={'home'}>
+            <div className={'intro'}></div>
             <div className={'container'}>
 
             </div>
